85b4076979d286b1ead13cdb13bbcdd57c5104b3
martin-coutts <martinc@traveltek.net>
2020-04-14 17:58:36 +0100

    Removed boilerplate and fixed hot reloading

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Placeholder</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+}
+</style>
