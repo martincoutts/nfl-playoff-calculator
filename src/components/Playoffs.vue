dae58f57afeb781c90e76d1ff8f0091a3b4f3e8d
martin-coutts <martinc@traveltek.net>
2020-04-28 19:10:06 +0100

    Playoffs now calling sortConferences if no playoffs data exists on creation

--- src/components/Playoffs.vue
+++ src/components/Playoffs.vue
@@ -8,15 +8,32 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 import PlayoffConference from "./PlayoffConference";
 import Superbowl from "./Superbowl";
 export default {
   name: "Playoffs",
+  // data: function() {
+  //   return {
+  //     ...mapState(["results"])
+  //   };
+  // },
   components: { PlayoffConference, Superbowl },
   computed: {
-    ...mapState(["afcPlayoffs", "nfcPlayoffs", "playoffsCombined"])
+    ...mapState(["results", "afcPlayoffs", "nfcPlayoffs", "playoffsCombined"])
+  },
+  created: function() {
+    if (!this.playoffsCombined.length) {
+      this.sortConferences({ conference: "AFC", conferenceArray: "afc" });
+      this.sortConferences({
+        conference: "NFC",
+        conferenceArray: "nfc"
+      });
+    }
+  },
+  methods: {
+    ...mapActions(["sortConferences"])
   }
 };
 </script>
