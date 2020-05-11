bd720c6a8edde3baf2d242772f9b55395ba20c7d
martin-coutts <martinc@traveltek.net>
2020-05-11 18:12:58 +0100

    Added simple error page if results come back with error

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="error-page">
+    <h1>Sorry, something went wrong...</h1>
+    <span>Please try again later</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorPage"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/index.scss";
+.error-page {
+  height: 100vh;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
+}
+</style>
